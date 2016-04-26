f4437f340ae022dd6df553049c41f47af22fbcdb
Anuraj <anurajsingh1709@gmail.com>
2016-04-26 19:31:06 +0530

    Added Submission

--- confrence/admin.py
+++ confrence/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from confrence.models import Confrence, Reviewr, Topic, Author
+from confrence.models import Confrence, Reviewr, Topic, Author, Upload, Submission
 
 # Register your models here.
 admin.site.register(Confrence)
 admin.site.register(Reviewr)
 admin.site.register(Topic)
 admin.site.register(Author)
+admin.site.register(Upload)
+admin.site.register(Submission)
